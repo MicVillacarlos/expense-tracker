b0e2c9468708f49502e7d48bc43f8fb5c117b2d4
MicVillacarlos <micvillacarlos@gmail.com>
2025-02-15 15:22:52 +0800

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import "./App.css";
-import TableTracker from "./components/organims/table/TableTracker";
+import Home from "./pages/home/Home";
 
 function App() {
-  const dummyItems = [
-    { id: 1, name: "Laptop", category: "Electronics", price: "$1200" },
-    { id: 2, name: "Coffee Maker", category: "Appliances", price: "$80" },
-    { id: 3, name: "Gaming Chair", category: "Furniture", price: "$250" },
-    { id: 4, name: "Smartphone", category: "Electronics", price: "$900" },
-    { id: 5, name: "Backpack", category: "Accessories", price: "$60" },
-  ];
 
   return (
     <>
-      <TableTracker items={dummyItems} />
+      <Home/>
     </>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import TableTracker from "@/components/organims/table/TableTracker";
+
+const Home = () => {
+  const dummyItems = [
+    { id: 1, name: "Laptop", category: "Electronics", price: "$1200" },
+    { id: 2, name: "Coffee Maker", category: "Appliances", price: "$80" },
+    { id: 3, name: "Gaming Chair", category: "Furniture", price: "$250" },
+    { id: 4, name: "Smartphone", category: "Electronics", price: "$900" },
+    { id: 5, name: "Backpack", category: "Accessories", price: "$60" },
+  ];
+  return <TableTracker items={dummyItems} />;
+};
+
+export default Home;
